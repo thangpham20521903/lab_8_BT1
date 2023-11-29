928f6f2b9d87a3fcaebb13754ae8969fac5ff5e2
thangpham20521903 <78943642+thangpham20521903@users.noreply.github.com>
2023-11-29 09:56:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ if data_file is not None:
   df = pd.read_csv(data_file)
   st.header("Show data")
   st.dataframe(df)
+  
   st.header('Descriptive statistics')
   st.table(df.describe())
+  
   st.header('Show data information')
   buffer = io.StringIO()
   st.write(buf=buffer)
   st.text(buffer.getvalue())
+  
   st.header("Visualize each atrribute")
   for col in list(df.columns):
     fig, ax = plt.subplots()
@@ -27,7 +30,20 @@ if data_file is not None:
     plt.xlabel(col)
     plt.ylabel('Quanity')
     st.pyplot(fig)
+
+  Output = st.radio('Choose a dependent variables: ', df.columns)
+  st.header('Show correlation')
+  for col in list(df.columns):
+    if col != Output:
+      fig, ax = plt.subplots()
+      ax.scatter(x=df[col], y=df[Output])
+      plt.xlabel(col)
+      plt.ylabel(Output)
+      st.pyplot(fig)
+
+  
   st.header('Show correlation between varibles')
   fig, ax = plt.subplots()
   sns.heatmap(df.corr(method='pearson'), ax=ax, vmax=1, square=True, anot=True, cmap='Reds')
   st.write(fig)
+  
